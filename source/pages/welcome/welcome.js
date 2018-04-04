b82473551edbbb76573ca68d5b31fb165afa868d
max <qqq@safe.ad>
2018-04-04 15:10:16 +0300

    web gl

--- source/pages/welcome/welcome.js
+++ source/pages/welcome/welcome.js
@@ -8,6 +8,8 @@ import '../about/hero.scss';
 import '../common/footer.scss';
 import '../about/aboutme.scss';
 
+import './index-water.js';
+
 $('.auth-button').on('click', e =>  {
    e.preventDefault;
    $('.flip__card').toggleClass('flipped');
