ef1188c28d67ef9668ffc6ef8ae7ae4955d93fae
max <qqq@safe.ad>
2018-04-04 19:13:51 +0300

    add parallax

--- source/pages/about/about.js
+++ source/pages/about/about.js
@@ -18,8 +18,24 @@ window.initMap = function(){
     });
 }
 
+function parallax(obj, speed) {
+    var scrolled = - window.pageYOffset || document.documentElement.scrollTop;
+    var translateY = scrolled / speed + "px";
+    obj.css('transform', 'translate3d(0, ' + translateY + ', 0');
+}
+
 window.onscroll = function() {
     var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    var hero = $('.hero_bg');
+    parallax(hero, 60);
+
+    var sectionTitle = $('.hero__title-block');
+    parallax(sectionTitle, 5);    
+    
+    var user = $('.user__block');
+    parallax(user, 3);
+
     var skills = document.querySelectorAll('.skill');
     skills.forEach((item, i)  => {
         var svgTop = item.getBoundingClientRect().top;        
